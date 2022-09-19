8a0c764510f1fdf27ceca5de30e556fffdcb1d2d
ermanjunaidi <ermanjunaidi1@gmail.com>
2022-09-19 22:57:49 +0700

    edit promise

--- promise.js
+++ promise.js
@@ -1,7 +1,25 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let bioskopMarah = 0
+let bisokopTidakMarah = 0
+const promiseOutput = emosi => {
+  if (emosi === "marah") {
+    return promiseTheaterIXX()
+      .then((value) => {
+        value.forEach(array => {
+          if (array.hasil === 'marah') {
+            bioskopMarah++
+          } else if (array.hasil === "tidak marah") {
+            bisokopTidakMarah++
+          } else {
+            'error'
+          }
+        });
+        return bioskopMarah
+      })
+  }
+};
 
 module.exports = {
   promiseOutput,
